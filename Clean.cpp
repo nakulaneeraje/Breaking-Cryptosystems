2962b30a96bb6b8c43eb19f96ef69ffaab57536e
nakulaneeraje <58438354+nakulaneeraje@users.noreply.github.com>
2021-05-17 15:43:42 +0530

    Update Clean.cpp

--- Clean.cpp
+++ Clean.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  char ch[100],temp[100];
+  FILE *fi,*fo;
+  fi = fopen("output2.txt","r+");
+  fo = fopen("output_clean2.txt","w+");
+  long int i = 0;
+  while(i < 200000)
+  {
+    int flag = 0;
+    //strcpy(ch,temp);
+    fscanf(fi,"%s",ch);
+    if(strlen(ch) == 16)
+    {
+      //cout << ch << "\n";
+        fprintf(fo, "%s\n",ch);
+        //cout << "yo" ;
+        i++;
+    }
+  }
+}
+
 
